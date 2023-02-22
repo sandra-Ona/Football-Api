d4a2ecf109dc8e0a9ff71fa2be12d90bf6b2ac35
sandra <sandraonachukwu@gmail.com>
2023-02-22 12:34:28 +0100

    adding number Filters

--- controller/teamController.js
+++ controller/teamController.js
@@ -2,7 +2,7 @@ const Team = require('../model/team')
 
 const getAllTeams = async (req, res) => {
     //req query
-   const  {name, location, uclwinner, sort, select} = req.query
+   const  {name, location, uclwinner, sort, select, numberFilters} = req.query
    let queryObject = {};
    let result = Team.find(queryObject)
   
@@ -15,6 +15,31 @@ const getAllTeams = async (req, res) => {
    if (uclwinner) {
     queryObject.uclwinner = uclwinner === 'true'? true: false;
    }
+   if (numberFilters){
+    //> >= < <= rating> 4.0 -rating $gt 4.0
+    // $gt $gte $eq $lt $lte
+    const operatorMap ={
+        ">": '$gt',
+        ">=": '$gte',
+        "=": '$eq',
+        "<": '$lt',
+        "<=": '$lte'
+    }
+     const regEx = /\b(<|>|>=|<=|=)\b/g; 
+     //ratings > 3.0
+     let filters= numberFilters.replace(regEx, (match)=> `-${operatorMap[match]}-`);
+     const options = ['rating']
+     filters= filters.split(',').forEach((item) => {
+        const [search, operator, value] = item.split ('-')
+        if (options.includes(search)) {
+            //queryObject['rating]= {[$gte]: 3.0}
+            queryObject[search]={ [operator]: Number(value)}
+        }
+     }) 
+        
+     };
+
+   
     //sorting
     if (sort){
         const sortList = sort.split(",").join (" ")
@@ -26,7 +51,7 @@ const getAllTeams = async (req, res) => {
         result=result.select(selectList)
      }
     //limit
-  const limit = number(req.query.limit)
+  const limit = Number(req.query.limit)
   result=result.limit(limit)
 
    result=result.find(queryObject) 
