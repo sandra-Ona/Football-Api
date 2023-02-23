72fd2959bce7a1df823d40858a8099a310843dc9
sandra <sandraonachukwu@gmail.com>
2023-02-23 12:35:32 +0100

    error code

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,5 +1,5 @@
 const notFound= (req, res) =>{
-    res.status(404).send('route not found')
+    res.status(404).send(`Route not found, try <a href = '/api/v1/teams' >FOOTBALL API </a>`)
 }
 
 module.exports = notFound;
